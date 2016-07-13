168517a4e6ef7ee02a1b0e969d55d78b9cfd2ee7
jiulanshu1221 <1123269386@qq.com>
2016-07-13 17:42:55 +0800

    swift 练习
    oneway

---
+++ OneDaySwift/OneDaySwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneDaySwift
+//
+//  Created by 吕莎莎 on 16/7/13.
+//  Copyright © 2016年 JiuLanShu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
